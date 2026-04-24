813cecd8f08149c99f78abd7579bd4d1f38a818a
Dae_Kang <eorhkd7078@naver.com>
2026-04-24 12:19:44 +0900

    basic_setting

---
+++ First_Aid_VIsion_RAG/backend/run_backend.bat
@@ -0,0 +1,10 @@
+:: 1. 가상환경 활성화 스크립트 실행
+call "F:\First_Aid_Vision_RAG\First_Aid_VIsion_RAG\backend\.venv\Scripts\activate.bat"
+
+:: 2. 프로젝트 소스 코드가 있는 디렉토리로 이동
+cd /d "F:\First_Aid_Vision_RAG\First_Aid_VIsion_RAG\backend"
+
+:: 3. FastAPI 서버 실행
+@REM uvicorn app.main:app --reload
+
+@REM pause
